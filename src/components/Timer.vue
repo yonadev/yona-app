53c0ccb6f499c219eb180940535228536cbe72f6
arienvanhelden <arienvanhelden@hotmail.com>
2019-04-03 12:36:13 +0200

    Fixed ts error

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -41,7 +41,7 @@
     }
   }
 
-  Vue.filter('twoDigits', function (value) {
+  Vue.filter('twoDigits', function (value: any) {
     if(value.toString().length <= 1)
       return "0"+value.toString();
 
