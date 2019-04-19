74e26fbf6f8bf2241666e5cb18a68a262476b177
Anton Vink <antonvink@gmail.com>
2019-04-19 16:21:29 +0200

    - Profile page

--- src/views/Me/Profile.vue
+++ src/views/Me/Profile.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    Your profile settings and badges
+  <div id="profile" class="colored-background purple-dark profile-template">
+    <div class="heading" v-if="page === 'profile'">
+      <div class="nav-title" v-if="edit">
+        ACCOUNT BEWERKEN
+        <img class="nav-icon" src="../../assets/images/signup/permission/icon_check_white.svg" alt="icn_edit" @click="switchMode">
+      </div>
+
+      <div class="nav-title" v-else="">
+        <img class="nav-icon" src="../../assets/images/profile/icn_edit.svg" alt="icn_edit" @click="switchMode">
+      </div>
+
+      <div class="wrapper">
+        <img class="profile-img" :src="require('../../assets/images/users/' + profilePic )" />
+        <p class="icon-title">
+          {{name}}
+        </p>
+        <p class="icon-text">
+          {{nickname}}
+        </p>
+      </div>
+    </div>
+
+    <div class="heading badges" v-else="">
+      <div class="nav-title"></div>
+      <div class="wrapper">
+        <img class="profile-img" :src="'../../assets/images/users/' + profilePic "/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +37,12 @@ import {Prop, Watch} from 'vue-property-decorator'
 
 @Component({})
 export default class Profile extends Vue {
-    //@Prop() msg: string = '';
+    private edit: boolean = false;
+    private page: string = 'profile';
+    name: string = 'Maria Schuurman';
+    nickname: string = 'Mamaria';
+    profilePic: string = 'maria1.jpg';
+    mobile: string = '';
 
     //Cycle hooks
     mounted () {
@@ -23,10 +54,12 @@ export default class Profile extends Vue {
     }
 
     //Methods
-    methodFunction () {
+    switchMode () {
+      this.edit = !(this.edit != false)
     }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+
 </style>
