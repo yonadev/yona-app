6f464c93a172edd480af9a47901369311e948425
arienvanhelden <arienvanhelden@hotmail.com>
2019-06-27 11:40:35 +0200

    Fix wrong urls in interceptor

--- src/utils/axios/axios.ts
+++ src/utils/axios/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import store from "../../store"
+import store from "@/store"
 
 let instance = axios.create({
   timeout: 1000,
@@ -13,8 +13,8 @@ let instance = axios.create({
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
   // Do something before request is sent
-  if(store.getters['links/yonaPassword'])
-    config.headers["Yona-Password"] = store.getters['header/yonaPassword']
+  if(store.getters['api/yonaPassword'])
+    config.headers["Yona-Password"] = store.getters['api/yonaPassword']
 
   return config;
 }, function (error) {
@@ -27,14 +27,14 @@ instance.interceptors.response.use(function (response) {
 
   // Do something with response data
   if(response.data.yonaPassword) {
-    store.dispatch("links/setHeaderPassword", {yonaPassword: response.data.yonaPassword});
+    store.dispatch("api/setHeaderPassword", {yonaPassword: response.data.yonaPassword});
 
     if(response.data._links){
-      store.dispatch("links/setLinks", {links: response.data._links})
+      store.dispatch("api/setLinks", {links: response.data._links})
     }
 
     if(response.data._embedded){
-      store.dispatch("links/setEmbedded", {embedded: response.data._embedded})
+      store.dispatch("api/setEmbedded", {embedded: response.data._embedded})
     }
   }
 
