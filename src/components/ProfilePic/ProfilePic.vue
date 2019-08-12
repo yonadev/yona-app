fd14bdd82210500c4e6132de0f6cf2ec7bcab0e8
Anton Vink <anton@b2it.nl>
2019-08-12 12:45:17 +0200

    -- Add support for profilePic data

--- src/components/ProfilePic/ProfilePic.vue
+++ src/components/ProfilePic/ProfilePic.vue
@@ -31,14 +31,22 @@ import { Component, Prop } from "vue-property-decorator";
 })
 export default class ProfilePic extends Vue {
   @Prop() src!: string;
+  @Prop() data!: {
+    type: string,
+    text: string
+  }
 
   pic_data: {} = {};
 
   mounted() {
-    const pic_src = window.localStorage.getItem(this.src);
+    if(this.src) {
+      const pic_src = window.localStorage.getItem(this.src);
 
-    if (pic_src !== null) {
-      this.pic_data = JSON.parse(pic_src);
+      if (pic_src !== null) {
+        this.pic_data = JSON.parse(pic_src);
+      }
+    } else if(this.data) {
+      this.pic_data = this.data;
     }
   }
 }
